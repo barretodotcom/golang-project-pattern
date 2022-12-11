b304787e2b2f12b922bd0bae9cdfdaff8bed6381
Lucas Barreto <lucas_barreto29@outlook.com>
2022-12-11 04:25:05 -0300

    Added PUT route

--- api/controller/students/put.go
+++ api/controller/students/put.go
@@ -1,28 +1,45 @@
 package students
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-project-pattern/api/controller"
+	"github.com/golang-project-pattern/api/controller/infra/database"
 	"github.com/golang-project-pattern/api/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func Put(c *gin.Context) {
 	var student model.Student
-
+	var newStudentParameters model.Student
 	id := c.Param("id")
 
-	c.BindJSON(&student)
-
-	for i, v := range model.Students {
-		if v.ID == id {
-			student.ID = v.ID
-			model.Students[i] = student
-		}
+	studentExists := database.DB.FindOne(context.TODO(), bson.D{{Key: "id", Value: id}})
+	if studentExists.Err() == mongo.ErrNoDocuments {
+		c.JSON(http.StatusBadGateway, controller.NewResponse("Student doesn't exists.", "error"))
+		return
 	}
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Update made with success.",
-		"status":  "ok",
-	})
+
+	err := c.BindJSON(&newStudentParameters)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, controller.NewResponse("Invalid request body: Not a JSON.", "error"))
+		return
+	}
+
+	studentExists.Decode(&student)
+
+	student.Name = newStudentParameters.Name
+
+	database.DB.FindOneAndUpdate(context.TODO(), bson.D{{Key: "id", Value: student.ID}}, 
+	bson.D{   
+		{           Key: "$set", Value: bson.D{{Key: "name", Value: student.Name}}}}
+              
+          )
+
+	c.JSON(http.StatusNoContent, controller.NewResponse("Updated.", "success."))
 	return
 }
